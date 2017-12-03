9b9c206be79e65905208dd43a71fc9297cdec2db
Balaraman <balaramanmuthupandi@gmail.com>
2017-12-03 09:33:23 +0530

    Added UI and Server

--- microservices/api/src/main.js
+++ microservices/api/src/main.js
@@ -1,8 +1,81 @@
+function compandchange(stn_tc, spots) {
+		// console.log(stn_tc);
+		var small = {}
+		var tc_small = 999.0;
+		for(var i=0; i<10; ++i) {
+			if((Math.abs(stn_tc - spots[i].tc)) < tc_small) {
+				small = spots[i];
+				tc_small = Math.abs(stn_tc - spots[i].tc);
+			}
+		}
+		$('#sname').html(small.name);
+		$('#rating').html(small.pop);
+		$("#rhead").show();
+		return small
+}
+
 function main() {
 	console.log("Yes");
 	$('#go').on('click', function(){
 		console.log('Go');
+		var spots = {}
+		var next = {}
+		//Get spot's TC
+		$.ajax({
+			url: "https://data.convalesce34.hasura-app.io/v1/query",
+			contentType: "application/json",
+			data: JSON.stringify({
+		      "type": "select",
+		      "args": {
+		            "table": "spots",
+		            "columns": [
+		                  "*"
+		            ]
+		      }
+			}),
+			type: "POST",
+			dataType: "json"
+		}).done(function(json) {
+			console.log(json);
+			spots = json
+		}).fail(function(xhr, status, errorThrown) {
+			console.log("Error: " + errorThrown);
+			console.log("Status: " + status);
+			console.dir(xhr);
+		});
+
+		//Get station's TC
+		$.ajax({
+			url: "https://data.convalesce34.hasura-app.io/v1/query",
+			contentType: "application/json",
+			data: JSON.stringify({
+			    "type": "select",
+			    "args": {
+			        "table": "stations",
+			        "columns": [
+			            "tc"
+			        ],
+			        "where": {
+			            "name": {
+			                "$eq": $("#stn_list").val()
+			            }
+			        }
+			    }
+			}),
+			type: "POST",
+			dataType: "json"
+		}).done(function(json) {
+			console.log(json);
+			next = compandchange(json[0].tc, spots);
+			console.log(next);
+		}).fail(function(xhr, status, errorThrown) {
+			console.log("Error: " + errorThrown);
+			console.log("Status: " + status);
+			console.dir(xhr);
+		});
 	});
 	
 }
+
+
 $(document).ready(main);
--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var bodyParser = require('body-parser');
-var fetchAction =  require('fetch');
 
 var app = express();
 app.use(morgan('combined'));
@@ -16,8 +15,8 @@ app.get('/ui/theme.css', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'theme.css'));
 });
 
-app.get('/ui/div1.jpg', function(req, res) {
-	res.sendFile(path.join(__dirname, 'ui', 'div1.jpg'));
+app.get('/ui/div2.jpg', function(req, res) {
+	res.sendFile(path.join(__dirname, 'ui', 'div2.jpg'));
 });
 
 app.get('/ui/title.png', function(req, res) {
@@ -26,6 +25,10 @@ app.get('/ui/title.png', function(req, res) {
 
 app.get('/main.js', function(req, res) {
 	res.sendFile(path.join(__dirname, 'main.js'));
+});
+
+app.get('/jquery.js', function(req, res) {
+	res.sendFile(path.join(__dirname, 'jquery.js'));
 });
 
 app.listen(8080, function() {
